8512fafdc6630ecd3f2cb335677939972da55bbb
gemary <ducdang1206@gmail.com>
2021-01-20 13:54:08 +0700

    set up test env

---
+++ test/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <div>
+    <h2>React Boilerplate Component Demo</h2>
+  
+  </div>, 
+  document.getElementById('app')
+);
