b248bcdebafdef583cb26cb434bf015f909700fc
gemary <ducdang1206@gmail.com>
2021-01-20 07:39:39 -0800

    add some components

---
+++ test/src/skeleton/components/Circle.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Circle extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Circle;
---
+++ test/src/skeleton/types/Rectangle.type.ts
@@ -0,0 +1,4 @@
+type RectType ={
+    width:number,
+    heigth:number,
+}
